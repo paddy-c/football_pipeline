065e06d7b0cc260b216b8f0157385821420d656e
Padraig Cleary <padraigcleary@gmail.com>
2023-08-15 18:54:02 +0100

    wip commit; sketching out some initial dbt model workflows

---
+++ dbt_transformations/models/lineups_and_managers/nested_lineups_and_managers.sql
@@ -0,0 +1,16 @@
+SELECT
+    date,
+    home_team,
+    away_team,
+    home_manager,
+    away_manager,
+    array_agg(home_player) as home_starting_11,
+    array_agg(away_player) as away_starting_11
+FROM
+    source( {{ 'fb_ref', 'football_lineups_and_managers'}} )
+group by
+    date,
+    home_team,
+    away_team,
+    home_manager,
+    away_manager
--- dbt_transformations/models/match_results_and_managers.sql
+++ dbt_transformations/models/match_results_and_managers.sql
@@ -1,9 +1,15 @@
 {{ config(materialized='table') }}
 
-with managers
-as (
-select distinct date, home_team as home_fbref, home_manager, away_team as away_fbref, away_manager from
-"football_lineups_and_managers"
+with managers as
+(
+select distinct
+    date,
+    home_team as home_fbref,
+    home_manager,
+    away_team as away_fbref,
+    away_manager
+from
+    {{ source( 'fb_ref', 'football_lineups_and_managers') }}
 )
 
 -- joining on home team and date *should* be sufficient - this maps to a unique match

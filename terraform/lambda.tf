a6eeb2875948810efea9748bcd113c56fbd5e3b5
Padraig Cleary <padraigcleary@gmail.com>
2023-08-27 21:13:01 +0100

    Add cleaning lambda for football-data-co-uk raw files

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -10,6 +10,16 @@ resource "aws_lambda_function" "football_data_co_uk_loader" {
   timeout = "900"
 }
 
+resource "aws_lambda_function" "clean_football_data_co_uk" {
+  function_name = "clean_football_data_co_uk"
+  image_uri     = "${aws_ecr_repository.football_repository.repository_url}:aws_ingestion_lambda_tasks"
+  package_type = "Image"
+  image_config {
+    command = ["football_data_co_uk.clean_footballdata_handler"]
+  }
+  role    = aws_iam_role.lambda_ex.arn
+}
+
 # Pandas-based util that appended all partition csvs into single dataframe, which resolves column alignment issues
 resource "aws_lambda_function" "consolidate_footballdata_lambda" {
   function_name = "consolidate_footballdata_lambda"
@@ -47,3 +57,17 @@ resource "aws_lambda_function" "team_lineups_loader" {
   timeout = "30"
 
 }
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.football_data_co_uk_raw.id
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.clean_football_data_co_uk.arn
+    events              = ["s3:ObjectCreated:*"]  # Triggered when an object is created. Adjust event types as necessary.
+    filter_suffix = ".csv"
+    # You can also specify filters like prefix/suffix if needed:
+    # filter_prefix       = "logs/"
+    # filter_suffix       = ".log"
+  }
+}
+
+
---
+++ terraform/s3.tf
@@ -0,0 +1,3 @@
+resource "aws_s3_bucket" "football_data_co_uk_raw" {
+  # Configuration here
+}

1353a81bb6d9b70030fd4bdc7d36150b93e47243
Padraig Cleary <padraigcleary@gmail.com>
2023-08-31 15:32:07 +0100

    Add combined results,xg,prices model and add fbref:football-data-co-uk team name mappings seed file

--- dbt_transformations/models/staging/stg_expected_goals_results.sql
+++ dbt_transformations/models/staging/stg_expected_goals_results.sql
@@ -2,9 +2,25 @@
   config(materialized='view')
 }}
 
--- TODO: good opportunity to carry out column renaming here...
 select
-  *
+  week,
+  day,
+  date,
+  time as time_fbref,
+  home,
+  away,
+  score,
+  attendance,
+  venue,
+  referee as referee_fbref,
+  home_goals,
+  away_goals,
+  home_xg,
+  away_xg,
+  league as league_fbref,
+  season as season_fbref
+
+
 from
   {{ source('fb_ref', 'football_xg_results_clean') }}
 where

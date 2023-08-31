2ffb5a852014cfee91200e38af3e68d42fb9350e
Padraig Cleary <padraigcleary@gmail.com>
2023-08-31 15:32:07 +0100

    Add combined results,xg,prices model and add fbref:football-data-co-uk team name mappings seed file

--- dbt_transformations/models/staging/stg_results_and_prices.sql
+++ dbt_transformations/models/staging/stg_results_and_prices.sql
@@ -3,9 +3,10 @@
 }}
 
 -- TODO: good opportunity to carry out column renaming here...
+-- TODO: add case statement to parse dates for non epl games 
 select
   *
 from
-  {{ source('football_data_co_uk', 'results_and_odds_country_england') }}
+  {{ source('football_data_co_uk', 'results_and_odds_new_country_england') }}
 where
   (fthg is not null) AND (ftag is not null) -- check for null rows

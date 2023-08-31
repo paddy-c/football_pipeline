4294d4dffb3d43906a06fa8c5788c447689755e2
Padraig Cleary <padraigcleary@gmail.com>
2023-08-31 21:52:18 +0100

    Add team-level combined data model

---
+++ dbt_transformations/models/combined/england_combined_results_xg_prices.sql
@@ -0,0 +1,48 @@
+{{ config(materialized='table') }}
+-- TODO: implement incremental refresh later
+
+with 
+home_team_mapping as
+(
+select 
+  fb_ref_name as home, 
+  football_data_name as hometeam 
+from 
+  {{ ref("fb_ref_football_data_co_uk_mapping")}}
+),
+
+away_team_mapping as
+(
+select 
+  fb_ref_name as away, 
+  football_data_name as awayteam 
+from 
+  {{ ref("fb_ref_football_data_co_uk_mapping")}}
+),
+
+xg_results_with_fd_names as (
+select * from
+{{ ref('stg_expected_goals_results')}} xg 
+left join 
+  home_team_mapping 
+using 
+  (home)
+left join 
+  away_team_mapping
+using
+  (away)
+)
+ 
+  
+select 
+  *
+from 
+  {{ ref('stg_results_and_prices') }} results_fd
+left join
+  xg_results_with_fd_names xg
+using(date, hometeam, awayteam)
+--on xg.home_fd = results_fd.hometeam
+--and xg.away_fd = results_fd.awayteam 
+--and xg.date = results_fd.date 
+where 
+  div in ('E0','E1','E2','E3')
